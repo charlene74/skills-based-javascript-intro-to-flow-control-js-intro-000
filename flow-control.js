3a121459a6953804b4e38234e2b64cc02c20446b
CHARLENE GREEN <charlenecgreen@gmail.com>
2019-02-01 02:23:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,7 +4,7 @@ return "You are a teenager!";
 }
 
 function teenager(age) {
-if (age === '13','14','15','16','17','18','19') {
+if (age >=13 && age <=19) {
  return "You are a teenager!";
 
 } else {
